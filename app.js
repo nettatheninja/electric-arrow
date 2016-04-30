5dc333c92f7d80ebb52c94db79d6c15ec29007c6
Netta Marshall <netta@nettamarshall.com>
2016-04-30 15:43:27 -0700

    adding other files

---
+++ app.js
@@ -0,0 +1,273 @@
+(function() {
+	quotes = {
+		"ash-ponders": {
+			"name": "Ash Ponders",
+			"title": "Photographer",
+			"link": "https://about.me/ash",
+			"quote": [
+				"Read way more; go find folks who’ve already done it, look them in the eye, learn how they did it, realize they’re human too."
+			]
+		},
+
+		"vlad-georgescu": {
+			"name": "Vlad Georgescu",
+			"title": "Designer",
+			"link": "http://vladgeorgescu.com",
+			"quote": [
+				"Don't be afraid to ask others for advice/feedback. Write down the things going through your mind. Always sign a contract."
+			]
+		},
+
+		"ben-myers": {
+			"name": "Ben Myers",
+			"title": "Designer",
+			"link": "https://robotsandpencils.com",
+			"quote": [
+				"Don't be afraid to go up to somebody and just talk to them if you want to meet them."
+			]
+		},
+
+		"diana-kimball": {
+			"name": "Diana Kimball",
+			"title": "Product Manager",
+			"link": "http://blog.dianakimball.com",
+			"quote": [
+				"Try it ten different ways."
+			]
+		},
+
+		"dustin-senos": {
+			"name": "Dustin Senos",
+			"title": "Designer",
+			"link": "https://medium.com/@dustin",
+			"quote": [
+				"If I could go back and do this again, I'd force myself to write all. the. time."
+			]
+		},
+
+		"zach-cole": {
+			"name": "Zach Cole",
+			"title": "Designer",
+			"link": "http://zcole.me",
+			"quote": [
+				"To not let the fear of being a beginner hold you back from sharing what you're working on publicly."
+			]
+		},
+
+		"justin-severson": {
+			"name": "Justin Severson",
+			"title": "Writer",
+			"link": "http://www.justinseverson.com",
+			"quote": [
+				"Trust in your abilities, and don’t be afraid of never starting something because you don’t feel good enough."
+			]
+		},
+
+		"scott-perket": {
+			"name": "Scott Perket",
+			"title": "Designer",
+			"link": "http://www.scottperket.com",
+			"quote": [
+				"Ask more questions than you answer, continually find mentors and nurture those relationships, study and practice mindfulness."
+			]
+		},
+
+		"rebecca-searles": {
+			"name": "Rebecca Searles",
+			"title": "Journalist",
+			"link": "http://www.rebeccasearles.com",
+			"quote": [
+				"You don't have to pick the right career path the first time. Get out there and try something. Move on if it's not right."
+			]
+		},
+
+		"max-temkin": {
+			"name": "Max Temkin",
+			"title": "Designer",
+			"link": "http://maxistentialism.com",
+			"quote": [
+				"Try things over and over again and throw away the bad ones, which is most of them.", "Also, pick your battles carefully, and ask yourself, \"is this the hill I want to die on?\" before you get into a fight."
+			]
+		},
+
+		"zach-inglis": {
+			"name": "Zach Inglis",
+			"title": "Designer",
+			"link": "http://www.zachinglis.com",
+			"quote": [
+				"Critique is not your enemy, it’s your best friend. Embracing it helps you make the best designs."
+			]
+		},
+
+		"tilly-woden": {
+			"name": "Tilly Woden",
+			"title": "",
+			"link": "https://twitter.com/TillyWoden",
+			"quote": [
+				"Finish things. You're not gonna get better by leaving unfinished stuff in your wake.", "Everybody stumbles blindly through life, bumping into things and bluffing their way past the hard bits. It's not just you."
+			]
+		},
+
+		"kevin-cole": {
+			"name": "Kevin Cole",
+			"title": "Game developer",
+			"link": "https://supertry.itch.io",
+			"quote": [
+				"Finish something! Only doing the first half of projects means you're only getting good at the first half."
+			]
+		},
+
+		"andrew-norcross": {
+			"name": "Andrew Norcross",
+			"title": "Developer",
+			"link": "http://andrewnorcross.com",
+			"quote": [
+				"Just start building things and putting it someplace public."
+			]
+		},
+
+		"matt-felten": {
+			"name": "Matt Felten",
+			"title": "Designer",
+			"link": "http://mattfelten.com",
+			"quote": [
+				"Sell your ideas ruthlessly, but be gracious to criticism. \"They just don't get it\" is your problem, not theirs."
+			]
+		},
+
+		"sean-kelley": {
+			"name": "Sean Kelley",
+			"title": "Writer",
+			"link": "http://www.frakkingshiny.blogspot.com",
+			"quote": [
+				"Always create, even if you're not in the mood. Some of your best work will come from days you didn't feel inspired."
+			]
+		},
+
+		"shannon-saar": {
+			"name": "Shannon Saar",
+			"title": "Cartoonist",
+			"link": "http://www.wighthousecomic.com/boundbysound/",
+			"quote": [
+				"block elements out first. the more you do things, the easier it gets. \"undo\" is not your friend."
+			]
+		},
+
+		"taylor-smith": {
+			"name": "Taylor Smith",
+			"title": "iOS Developer",
+			"link": "http://www.looprecur.com",
+			"quote": [
+				"Ignore your inner asshole. You are, in fact, good enough to do xyz, but like everyone else, you need to work through the hard."
+			]
+		},
+
+		"jake-barry": {
+			"name": "Jake Barry",
+			"title": "Designer",
+			"link": "http://jakebarry.us",
+			"quote": [
+				"Spend less time chasing all the shiny new technologies/trends/frameworks/etc. and more time making things!"
+			]
+		},
+
+		"cameron-daigle": {
+			"name": "Cameron Daigle",
+			"title": "Designer",
+			"link": "http://www.camerondaigle.com",
+			"quote": [
+				"Rapport is everything. If you understand someone, your critique will be more informed. If they trust you, they'll listen."
+			]
+		},
+
+		"robert-devore": {
+			"name": "Robert DeVore",
+			"title": "Designer",
+			"link": "https://twitter.com/deviorobert",
+			"quote": [
+				"Toss your work on github, blog about it, and continue to improve your skills every week."
+			]
+		},
+
+		"benedikt-lehnert": {
+			"name": "Benedikt Lehnert",
+			"title": "Designer",
+			"link": "http://www.benediktlehnert.com",
+			"quote": [
+				"Don’t be afraid. And be f’cking patient—you will get there, through continuous work and practice. Design is a job."
+			]
+		},
+
+		"jessica-paoli": {
+			"name": "Jessica Paoli",
+			"title": "Designer",
+			"link": "http://jessicapaoli.com",
+			"quote": [
+				"Stop idolizing + comparing your work to others. You have different experiences and that's what makes your perspective special."
+			]
+		},
+
+		"dan-higbie️": {
+			"name": "Dan Higbie️",
+			"title": "Designer",
+			"link": "http://www.danhigbie.com",
+			"quote": [
+				"Try a bit of everything. \“Design\” is a big world, you don’t have to be locked into others perceptions of what it means."
+			]
+		},
+
+		"joe-mills": {
+			"name": "Joe Mills",
+			"title": "Illustrator",
+			"link": "http://www.joemills.com",
+			"quote": [
+				"Use more of your free time for creative projects. Go to more art show openings and events. Talk to more people."
+			]
+		},
+
+		"roger-clark": {
+			"name": "Roger Clark",
+			"title": "Software Engineer",
+			"link": "http://rogerclark.online",
+			"quote": [
+				"Everything is a skill. There's no such thing as inbuilt talent. Ability comes from deliberate practice and your environment."
+			]
+		},
+
+		"daniel-fitzgerald": {
+			"name": "Daniel Fitzgerald",
+			"title": "Photographer",
+			"link": "http://www.lovely.business",
+			"quote": [
+				"Learn to separate the love you have for the craft from the stress of the business.", "Fundamentals matter more than any new technology. Focus on fundamentals for years longer than you feel like you should.", "Daily grinding is more important than breakthrough moments. Passion follows commitment."
+			]
+		},
+
+		"soleio": {
+			"name": "Soleio",
+			"title": "Designer",
+			"link": "facebook.com/soleio",
+			"quote": [
+				"Keep a holistic perspective.", "It’s not about you! Being right ≠ being effective.", "It is not the critic who counts."
+			]
+		},
+
+		"bruno-bergher": {
+			"name": "Bruno Bergher",
+			"title": "Designer",
+			"link": "http://brunobergher.com",
+			"quote": [
+				"I'd have emphasized learning over producing in the beginning. ROI on learning is front-loaded, like saving for retirement.", "Co-workers matter much, much more than industries, projects or compensation."
+			]
+		},
+
+		"cyd-harrell": {
+			"name": "Cyd Harrell",
+			"title": "Designer",
+			"link": "https://twitter.com/cydharrell",
+			"quote": [
+				"That massive anxiety you have about mistakes and getting stuck, let that go. try things. move on judiciously, but do move on.", "People will like you more, throughout your career, the more you are your full self and play to your own strengths."
+			]
+		},
+	};
+})();
