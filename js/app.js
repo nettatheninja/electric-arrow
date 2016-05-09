44eb68765f896a0999228f74cd1d5b99283fc01a
Netta Marshall <netta@nettamarshall.com>
2016-05-08 23:29:41 -0700

    loads of stuff... updated styles, updated queries, fixed navigation option on mobile, added mobile js, and unified font sizes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 (function(){
-	keyboardPagination( '#container', {
-		prev: '#pagination-prev',
-		next: '#pagination-next'
-	});
+	// SETUP
 	var href = window.location.href;
 	var name = href.substr(href.lastIndexOf('/') + 1);
 	var authors = window.nm.authors;
@@ -12,6 +9,7 @@
 		author = window.nm.quotes[name];
 	}
 
+	// TWEET BUTTON
 	document.querySelectorAll('.twitter-share-button')[0].onclick = function(event) {
 		var shareUrl = encodeURIComponent('http://' + window.location.host + '/' + name);
 		var tweetBody = encodeURIComponent('Check out this bit of advice by ' + author.name);
@@ -28,9 +26,10 @@
 				',left='   + left;
 
 		window.open(url, 'twitter', opts);
-
 		return false;
 	} 
+
+	// CONTENT
 	var quotesEl = document.querySelectorAll('#advice')[0];
 	quotesEl.innerHTML = '';
 	for (var i = 0; i < author.quote.length; i++) {
@@ -46,9 +45,23 @@
 	var creditProfEl = document.querySelectorAll('#credit span')[0];
 	creditProfEl.innerText = author.title;
 
+	// PAGINATION
 	var current = name.length ? authors.indexOf(name) : 0;
 	var prevAuthor = authors[(current + authors.length - 1) % authors.length];
 	document.querySelectorAll('#pagination-prev a')[0].href = '/' + prevAuthor;
 	var nextAuthor = authors[(current + authors.length + 1) % authors.length];
-	document.querySelectorAll('#pagination-next a')[0].href = '/' + nextAuthor;	
+	document.querySelectorAll('#pagination-next a')[0].href = '/' + nextAuthor;
+
+	keyboardPagination( '#container', {
+		prev: '#pagination-prev',
+		next: '#pagination-next'
+	});
+	if (window.mobile) {
+		var style = document.createElement('style');
+		style.innerHTML = 'div { cursor: pointer; }';
+		document.head.appendChild(style);
+		document.onclick = function(){
+			location.href = '/' + nextAuthor;
+		};
+	}
 })();
